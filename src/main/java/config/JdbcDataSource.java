33930427a8739f69000c5218afef2c4ddf8e1725
Heriata <heriata@yandex.ru>
2024-07-04 02:30:02 +0300

    lil fix

--- src/main/java/config/JdbcDataSource.java
+++ src/main/java/config/JdbcDataSource.java
@@ -11,7 +11,7 @@ public class JdbcDataSource {
 
     public static Connection getConnection() {
         Connection connection = null;
-        try {
+        try{
 //            Properties prop = new Properties();
 //            InputStream inputStream = JdbcDataSource.class.getClassLoader().getResourceAsStream("./config.properties");
 //            prop.load(inputStream);
@@ -24,11 +24,13 @@ public class JdbcDataSource {
 //            Class.forName(driver);
 //            connection = DriverManager.getConnection(url, user, password);
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection("jdbc:postgresql://192.168.100.11:5432/usersapp", "postgres", "");
+            connection = DriverManager.getConnection("jdbc:postgresql://192.168.100.11:5432/user_app", "postgres", "");
+
+            connection.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        if (connection == null) throw new RuntimeException("Connection to DB not established");
+        if (connection == null) throw new RuntimeException("Connection to DB has not established");
         return connection;
     }
 }
